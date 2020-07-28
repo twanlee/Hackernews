bfb71dcd2c4a52e922fe510688d0af5000bcb5c0
twanlee <levantuan6898@gmail.com>
2020-07-28 14:53:42 +0700

    add file bootstrap

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,14 @@ import {NgModule} from '@angular/core';
 import {AppRoutingModule} from './app-routing.module';
 import {AppComponent} from './app.component';
 import {FormsModule} from '@angular/forms';
+import { NavibarComponent } from './navibar/navibar.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavibarComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
